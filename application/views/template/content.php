6e0e9200b454f8f697e5af8fa1b0e03bb45c6261
Suvi Sanusi <suvi.7888@gmail.com>
2019-12-12 10:24:07 +0700

    ganti header

--- application/views/template/content.php
+++ application/views/template/content.php
@@ -7,7 +7,7 @@
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><img src="http://jdih.awanesia.com/logo-esdm-kuning.png"></span>
       <!-- logo for regular state and mobile devices -->
-      <span class="logo-lg"><b>BSC</b></span>
+      <span class="logo-lg"><b>DASHBOARD BLU</b></span>
     </a>
     <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
