e52c1cc680313259a4e6171d1ba126293fde89f0
Loetfi <lutfi@mhs.ubharajaya.ac.id>
2020-01-27 20:25:15 +0700

    Update chmod.php

--- chmod.php
+++ chmod.php
@@ -5,3 +5,7 @@ echo "<pre>$output</pre>";
 
 $open = shell_exec('chmod -R 777 /assets/json');
 echo "<pre>$p$</pre>";
+
+$buat = shell_exec('mkdir /assets/json/2020 && chmod -R 777 /assets/json/2020');
+
+echo '<pre>'. $buat . '</pre>';
